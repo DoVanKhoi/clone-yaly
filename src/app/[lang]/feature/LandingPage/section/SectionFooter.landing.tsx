e235e45dafc03632fddea9a03523f06605a24cc9
pikaden <phamngocnhan8@gmail.com>
2025-12-16 20:09:06 +0700

    update landing page

---
+++ src/app/[lang]/feature/LandingPage/section/SectionFooter.landing.tsx
@@ -0,0 +1,7 @@
+export default function SectionFooterLanding() {
+  return (
+    <>
+      <div className="flex justify-center m-16">chukapi</div>
+    </>
+  );
+}
---
+++ src/app/[lang]/feature/LandingPage/section/SectionHeader.landing.tsx
@@ -0,0 +1,7 @@
+export default function SectionHeaderLanding() {
+  return (
+    <>
+      <div className="flex justify-center m-16">pikachu</div>
+    </>
+  );
+}
