1f5345ba4d6df05329f23ebb4b558330b2b19e64
pikaden <phamngocnhan8@gmail.com>
2025-12-16 11:20:30 +0700

    fix proxy location

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { i18n } from "./utils/i18n-config";
+import { i18n } from "./app/utils/i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
