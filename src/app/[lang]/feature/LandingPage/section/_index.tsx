0e49a08cee129ceb55cc79c1736363e240c07f06
DoVanKhoi <khoifro2002@gmail.com>
2025-12-28 09:53:44 +0700

    add new section

--- src/app/[lang]/feature/LandingPage/section/_index.tsx
+++ src/app/[lang]/feature/LandingPage/section/_index.tsx
@@ -1,10 +1,14 @@
+import SectionPannelLanding from "./SectionPannel.landing";
 import SectionFooterLanding from "./SectionFooter.landing";
 import SectionHeaderLanding from "./SectionHeader.landing";
 import SectionIntroLanding from "./SectionIntro.landing";
+import SectionContentLanding from "./SectionContent.landing";
 
 const LANDING = {
   SectionHeaderLanding: SectionHeaderLanding,
   SectionIntroLanding: SectionIntroLanding,
+  SectionPannelLanding: SectionPannelLanding,
+  SectionContentLanding: SectionContentLanding,
   SectionFooterLanding: SectionFooterLanding,
 };
 
---
+++ src/public/images/index.ts
@@ -0,0 +1,6 @@
+export { default as Background1 } from "./img_1.jpg";
+export { default as Background2 } from "./img_2.jpg";
+export { default as Background3 } from "./img_3.jpg";
+export { default as Background4 } from "./img_4.jpg";
+export { default as Background5 } from "./img_5.jpg";
+export { default as ImgContent1 } from "./img_6.webp";
